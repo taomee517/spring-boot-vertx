ea893041cc44ff0e5660fdec09e1d49eca0d675c
Administrator <taomee517@gmail.com>
2020-04-10 20:04:55 +0800

    完成gradle手动解决依赖冲突案例，重点看最外层build.gradle

---
+++ spring-vertx-demo/src/main/java/org/example/vertx/SpringVertxApplication.java
@@ -0,0 +1,14 @@
+package org.example.vertx;
+
+/**
+ * SpringVertxApplication
+ *
+ * @Author: taomee
+ * @Date: 2020/4/9 0009 16:34
+ * @Description:
+ */
+public class SpringVertxApplication {
+    public static void main(String[] args) {
+
+    }
+}
