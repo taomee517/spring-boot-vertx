7e84a3d091db46a58f80e6af0523d3b19b20ee57
Administrator <taomee517@gmail.com>
2020-04-10 22:36:25 +0800

    统一配置日志，添加项目内依赖，解决编译乱码

--- spring-vertx-demo/src/main/java/org/example/vertx/SpringVertxApplication.java
+++ spring-vertx-demo/src/main/java/org/example/vertx/SpringVertxApplication.java
@@ -1,5 +1,9 @@
 package org.example.vertx;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * SpringVertxApplication
  *
@@ -7,8 +11,11 @@ package org.example.vertx;
  * @Date: 2020/4/9 0009 16:34
  * @Description:
  */
+@Slf4j
+@SpringBootApplication
 public class SpringVertxApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(SpringVertxApplication.class);
+        log.info("spring-vertx 项目启动！");
     }
 }
