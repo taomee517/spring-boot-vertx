a3b5bb7d8f285b99bc424ee62819f08d4ed2758e
Administrator <taomee517@gmail.com>
2020-04-12 15:27:46 +0800

    加入案例的数据库sql文件

---
+++ spring-vertx-demo/src/main/resources/sql/demo.sql
@@ -0,0 +1,32 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : local
+Source Server Version : 50729
+Source Host           : localhost:3306
+Source Database       : demo
+
+Target Server Type    : MYSQL
+Target Server Version : 50729
+File Encoding         : 65001
+
+Date: 2020-04-12 15:26:57
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for tb_book
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_book`;
+CREATE TABLE `tb_book` (
+  `id` int(11) NOT NULL,
+  `name` varchar(64) DEFAULT NULL,
+  `author` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tb_book
+-- ----------------------------
+INSERT INTO `tb_book` VALUES ('1', '围城', '钱钟书');
